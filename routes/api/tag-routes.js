8d85715f86670baf389f4bfa484481e942fb9c41
Peter O'Callaghan <peterocallaghan23@gmail.com>
2022-07-13 15:59:59 +1000

    added homework file and updated routes and models folders

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,91 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try{
+    const tags = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+        },
+      ]
+    });
+    res.json(tags)
+  } catch(error){
+    res.status(507).json(error)
+  }
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try{
+    const tagByID = await Tag.findOne({
+      where: {
+        include: Product,
+      },
+    })
+    if(tagByID){
+      res.json(tagByID)
+    }else{
+      res.status(404).json({ error: "No tag with this id" });
+    }
+  }catch(error){
+    res.status(509).json(error);
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  try{
+    const newTag = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.json(newTag)
+  }catch(error){
+    res.status(510).json(error);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  try{
+    const updatedTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+    if(updatedTag){
+      res.json(updatedTag)
+    }else{
+      res.status(404).json({ error: " No tag with this id"})
+    }
+  }catch(error){
+    res.status(511).json(error);
+  }
+
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  try{
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if(updatedTag){
+      res.json(updatedTag)
+    }else{
+      res.status(404).json({ error: "No tag with this id"})
+    }
+  }catch{
+    res.status(512).json(error)
+  }
+});
+
+module.exports = router;
